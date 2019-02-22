3ff1bf9e4046c06ea392c58e70669c5934b563b1
Hiroko Tamagawa <hiroko.tamagawa@trident-qa.com>
2019-02-22 12:19:49 +0900

    Fix detail problems for input

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/bitrise-io/go-utils/log"
 )
 
 func main() {
-	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
+	log.Infof("This is the value specified for the input 'target_type':", os.Getenv("target_type"))
 
 	//
 	// --- Step Outputs: Export Environment Variables for other Steps:
@@ -19,6 +21,7 @@ func main() {
 		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
 		os.Exit(1)
 	}
+
 	// You can find more usage examples on envman's GitHub page
 	//  at: https://github.com/bitrise-io/envman
 
@@ -27,5 +30,6 @@ func main() {
 	// The exit code of your Step is very important. If you return
 	//  with a 0 exit code `bitrise` will register your Step as "successful".
 	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	log.Donef("Success")
 	os.Exit(0)
 }
